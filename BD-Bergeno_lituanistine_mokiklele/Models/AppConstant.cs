7728d1b7472d15866ec16e9ae17058ea6dd5e977
Tomas Bance <tomas.bance@gmail.com>
2023-04-30 00:01:26 +0200

    Rearange menu

--- BD-Bergeno_lituanistine_mokiklele/Models/AppConstant.cs
+++ BD-Bergeno_lituanistine_mokiklele/Models/AppConstant.cs
@@ -1,4 +1,5 @@
-﻿using BD_Bergeno_lituanistine_mokiklele.Controls;
+﻿using Android.Graphics.Drawables;
+using BD_Bergeno_lituanistine_mokiklele.Controls;
 using BD_Bergeno_lituanistine_mokiklele.Views.Dashboard;
 using BD_Bergeno_lituanistine_mokiklele.Views.Startup;
 using BD_Bergeno_lituanistine_mokiklele.Views.WebViewPages;
@@ -36,19 +37,7 @@ namespace BD_Bergeno_lituanistine_mokiklele.Models {
                                     Icon = Icons.AboutUs,
                                     Title = "Naujienos",
                                     ContentTemplate = new DataTemplate(typeof(NewsPage)),
-                                },
-                                new ShellContent
-                                {
-                                    Icon = Icons.Dashboard,
-                                    Title = "Nario Dashboard",
-                                    ContentTemplate = new DataTemplate(typeof(NarysDashboardPage)),
-                                },
-                                new ShellContent
-                                {
-                                    Icon = Icons.AboutUs,
-                                    Title = "Nario Profilis",
-                                    ContentTemplate = new DataTemplate(typeof(ProfileDashboardPage)),
-                                },
+                                },                               
                                 new ShellContent
                                 {
                                     Icon = Icons.AboutUs,
@@ -60,6 +49,19 @@ namespace BD_Bergeno_lituanistine_mokiklele.Models {
                                     Icon = Icons.AboutUs,
                                     Title = "Grupės",
                                     ContentTemplate = new DataTemplate(typeof(GroupsPage)),
+                                },
+
+                                new ShellContent
+                                 {
+                                    Icon = Icons.AboutUs,
+                                    Title = "Jūsų Profilis",
+                                    ContentTemplate = new DataTemplate(typeof(ProfileDashboardPage)),
+                                },
+                                  new ShellContent
+                                {
+                                    Icon = Icons.Dashboard,
+                                    Title = "Nario Dashboard",
+                                    ContentTemplate = new DataTemplate(typeof(NarysDashboardPage)),
                                 },
 
                             }
@@ -91,19 +93,7 @@ namespace BD_Bergeno_lituanistine_mokiklele.Models {
                                     Icon = Icons.AboutUs,
                                     Title = "Naujienos",
                                     ContentTemplate = new DataTemplate(typeof(NewsPage)),
-                                },
-                                new ShellContent
-                                {
-                                    Icon = Icons.Dashboard,
-                                    Title = "Teacher Dashboard",
-                                    ContentTemplate = new DataTemplate(typeof(TeacherDashboardPage)),
-                                },
-                                new ShellContent
-                                {
-                                    Icon = Icons.AboutUs,
-                                    Title = "Teacher Profile",
-                                    ContentTemplate = new DataTemplate(typeof(ProfileDashboardPage)),
-                                },
+                                },                                
                                 new ShellContent
                                 {
                                     Icon = Icons.AboutUs,
@@ -115,6 +105,18 @@ namespace BD_Bergeno_lituanistine_mokiklele.Models {
                                     Icon = Icons.AboutUs,
                                     Title = "Grupės",
                                     ContentTemplate = new DataTemplate(typeof(GroupsPage)),
+                                },
+                                new ShellContent
+                                {
+                                    Icon = Icons.AboutUs,
+                                    Title = "Jūsų Profilis",
+                                    ContentTemplate = new DataTemplate(typeof(ProfileDashboardPage)),
+                                },
+                                new ShellContent
+                                {
+                                    Icon = Icons.Dashboard,
+                                    Title = "Teacher Dashboard",
+                                    ContentTemplate = new DataTemplate(typeof(TeacherDashboardPage)),
                                 },
                    }
                 };
@@ -144,19 +146,7 @@ namespace BD_Bergeno_lituanistine_mokiklele.Models {
                                     Icon = Icons.AboutUs,
                                     Title = "Naujienos",
                                     ContentTemplate = new DataTemplate(typeof(NewsPage)),
-                                },
-                                new ShellContent
-                                {
-                                    Icon = Icons.Dashboard,
-                                    Title = "Admin Dashboard",
-                                    ContentTemplate = new DataTemplate(typeof(AdminDashboardPage)),
-                                },
-                                new ShellContent
-                                {
-                                    Icon = Icons.AboutUs,
-                                    Title = "Admin Profile",
-                                    ContentTemplate = new DataTemplate(typeof(ProfileDashboardPage)),
-                                },
+                                },                                                              
                                 new ShellContent
                                 {
                                     Icon = Icons.AboutUs,
@@ -168,6 +158,18 @@ namespace BD_Bergeno_lituanistine_mokiklele.Models {
                                     Icon = Icons.AboutUs,
                                     Title = "Grupės",
                                     ContentTemplate = new DataTemplate(typeof(GroupsPage)),
+                                },
+                                 new ShellContent
+                                {
+                                    Icon = Icons.AboutUs,
+                                    Title = "Jūsų Profilis",
+                                    ContentTemplate = new DataTemplate(typeof(ProfileDashboardPage)),
+                                },
+                                 new ShellContent
+                                {
+                                    Icon = Icons.Dashboard,
+                                    Title = "Admin Dashboard",
+                                    ContentTemplate = new DataTemplate(typeof(AdminDashboardPage)),
                                 },
                    }
                 };
